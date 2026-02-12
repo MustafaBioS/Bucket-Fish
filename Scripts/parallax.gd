c3c479a852dd739317936a2ce7b00bc915930217
MustafaBioS <mustaafahany@gmail.com>
2026-02-12 23:41:15 +0200

    ADD - Parallax Layer

---
+++ Scripts/parallax.gd
@@ -0,0 +1,8 @@
+extends ParallaxBackground
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	scroll_base_offset.x += -100 * delta
